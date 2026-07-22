775d43f5888ce33d1797e1ec5cdcb2ec0f7fc4ac
CheRokeeBTW <d.evstigneev99@yandex.ru>
2026-07-22 19:52:41 +0300

    change db to prod

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,17 +5,17 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-}); //for develop
-
 // export const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// }); //for prod
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: Number(process.env.DB_PORT),
+// }); //for develop
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}); //for prod
